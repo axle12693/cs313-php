8a0f346036b0b4f06166b8b230ee095d7dcedde7
axle12693 <axle12693@gmail.com>
2019-04-30 16:31:12 -0600

    Stretech part 1

--- web/W2_Teach/login.php
+++ web/W2_Teach/login.php
@@ -1,5 +1,20 @@
 <?php include("header.php") ?>
-    <input type="button" value="Login as administrator">
-    <input type="button" value="Login as tester">
+<?php
+    session_start();
+    if (isset($_GET['admin']))
+    {
+        $_SESSION['hello'] = "Administrator";
+        echo 1;
+    }
+    else
+    {
+        $SESSION["hello"] = "Tester";
+        echo 2;
+    }
+?>
+    <form action="login.php">
+        <input type="button" value="Login as administrator" method="get" name="admin">
+        <input type="button" value="Login as tester" method="get" name="tester">
+    </form>
 </body>
 </html>
