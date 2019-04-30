fece5962558e29cbc6df505a6569e081eacf3d2a
axle12693 <axle12693@gmail.com>
2019-04-30 16:33:20 -0600

    Stretch part 1 fix?

--- web/W2_Teach/login.php
+++ web/W2_Teach/login.php
@@ -6,13 +6,17 @@
         $_SESSION['hello'] = "Administrator";
         echo 1;
     }
-    else
+    elseif (isset($_GET['tester']))
     {
         $SESSION["hello"] = "Tester";
         echo 2;
     }
+    else
+    {
+        echo 0;
+    }
 ?>
-    <form action="login.php">
+    <form action="login.php" method="get">
         <input type="button" value="Login as administrator" method="get" name="admin">
         <input type="button" value="Login as tester" method="get" name="tester">
     </form>
