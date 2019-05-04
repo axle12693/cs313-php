5508f128b95c3661514f6578e573ded2c04cb1f3
axle12693 <axle12693@gmail.com>
2019-05-04 14:11:08 -0600

    Assignment2_6

--- web/assignments.php
+++ web/assignments.php
@@ -3,5 +3,6 @@
 </head>
 <body>
   <?php require 'header2.php'; ?>
+  <h1 class="body_container">Links to assignments coming soon!</h1>
 </body>
 </html>
--- web/index.php
+++ web/index.php
@@ -1,7 +1,18 @@
-<?php require 'header1.php'; ?>
-  <title>My Home Page</title>
-</head>
-<body>
-  <?php require 'header2.php'; ?>
-</body>
+    <?php require 'header1.php'; ?>
+        <title>My Home Page</title>
+    </head>
+    <body>
+        <?php require 'header2.php'; ?>
+        <div class="body_container">
+            <h2>About Machine Learning</h2>
+            <div>
+                <h3>What is Machine Learning?</h3>
+                <p>Machine learning is an umbrella term for several families of statistical or other techniques 
+                   that can be programmed to analyze data and make predictions. In most cases, a machine 
+                   learning algorithm can be looked at as a black box into which data is fed. In the beginning, 
+                   the black box is "trained" on pre-existing data, and when that phase is complete, the goal 
+                   is to have it able to discover patterns and/or make predictions about future data.</p>
+            </div>
+        </div>
+    </body>
 </html>
