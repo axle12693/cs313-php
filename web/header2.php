150217a1e5c5dbd5631c4e8b9eacea49f7d1b716
axle12693 <axle12693@gmail.com>
2019-05-04 13:41:08 -0600

    Assignment2_5 fixed header align? 2

--- web/header2.php
+++ web/header2.php
@@ -1,9 +1,11 @@
 <header>
-    <div class="jumbotron">
-        <h1>Alex Bentley</h1>
-        <ul class="list-inline">
-            <li class="list-inline-item"><button class="btn btn-outline-success my-2 my-sm-0">Home</button></li>
-            <li class="list-inline-item"><button class="btn btn-outline-success my-2 my-sm-0">Assignments</button></li>
-        </ul>
+    <div id="header_container">
+        <div class="jumbotron">
+            <h1>Alex Bentley</h1>
+            <ul class="list-inline">
+                <li class="list-inline-item"><button class="btn btn-outline-success my-2 my-sm-0">Home</button></li>
+                <li class="list-inline-item"><button class="btn btn-outline-success my-2 my-sm-0">Assignments</button></li>
+            </ul>
+        </div>
     </div>
 </header>
