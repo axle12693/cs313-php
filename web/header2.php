aadb6a69f664c7dd665b34fde4adef9cd9532994
axle12693 <axle12693@gmail.com>
2019-05-04 13:31:54 -0600

    Assignment2_4

--- web/header2.php
+++ web/header2.php
@@ -2,8 +2,8 @@
     <div class="jumbotron">
         <h1>Alex Bentley</h1>
         <ul class="list-inline">
-            <li class="list-inline-item"><a href="index.php">Home</a></li>
-            <li class="list-inline-item"><a href="assignments.php">Assignments</a></li>
+            <li class="list-inline-item"><button class="btn btn-outline-success my-2 my-sm-0">Home</button></li>
+            <li class="list-inline-item"><button class="btn btn-outline-success my-2 my-sm-0">Assignments</button></li>
         </ul>
     </div>
 </header>
