33f984891d352017540dedf09a221e8f8e38e28b
axle12693 <axle12693@gmail.com>
2019-05-04 13:14:00 -0600

    Assignment 2_3

--- web/header2.php
+++ web/header2.php
@@ -1,8 +1,6 @@
 <header>
     <div class="jumbotron">
-        Alex Bentley
-    </div>
-    <div>
+        <h1>Alex Bentley</h1>
         <ul class="list-inline">
             <li class="list-inline-item"><a href="index.php">Home</a></li>
             <li class="list-inline-item"><a href="assignments.php">Assignments</a></li>
