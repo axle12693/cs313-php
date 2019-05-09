15bd3d18da0965ca4b0f2e0cdc6bda37e770203a
axle12693 <axle12693@gmail.com>
2019-05-09 16:24:17 -0600

    W3_Prove

--- web/assignments.php
+++ web/assignments.php
@@ -1,8 +1,8 @@
-<?php require '../header1.php'; ?>
+<?php require 'header1.php'; ?>
     <title>Assignments</title>
 </head>
 <body>
-  <?php require '../header2.php'; ?>
+  <?php require 'header2.php'; ?>
   <div id="body_container">
     <a href="week03_teach/index.php"> Week03 Teach</a><br>
     <a href="W03_Prove/index.php">Week03 Prove</a>
