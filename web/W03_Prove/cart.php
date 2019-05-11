1d708653d0acc2efe4cbd676bee1537a7d77098b
axle12693 <axle12693@gmail.com>
2019-05-11 14:59:05 -0600

    W3Prove

--- web/W03_Prove/cart.php
+++ web/W03_Prove/cart.php
@@ -4,10 +4,9 @@
     <title>Alex's House of Mythical Pets</title>
 </head>
 <body>
-  <?php require $_SERVER['DOCUMENT_ROOT'] . '/header2.php'; ?>
-  <div id="body_container">
-    
-  </div>
-  <button onclick="window.location.replace('index.php');" value="Back to Browsing">Back to Browsing</button>
+    <?php require $_SERVER['DOCUMENT_ROOT'] . '/header2.php'; ?>
+    <div id="body_container">
+        <button onclick="window.location.replace('index.php');" value="Back to Browsing">Back to Browsing</button>
+    </div>
 </body>
 </html>
--- web/W03_Prove/index.php
+++ web/W03_Prove/index.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/header1.php'; ?>
 
     <script src="script.js"></script>
@@ -7,21 +8,21 @@
   <?php require $_SERVER['DOCUMENT_ROOT'] . '/header2.php'; ?>
   <div id="body_container">
     <div id="items_list_container">
-      <div class="item" id="dr">
+      <div class="item" id="dr" style="background-color: <?php echo($_SESSION["ItemsList"]["dr"] ? "#CCFFCC" : "#FFFFFF");  ?>">
         <p>
           <img class="item_pic_left" src="/images/dragon.jpg">
           <h3>Dragon - $1,000,000</h3>
           This is text describing a dragon. Dragons are really cool!
         </p>
       </div>
-      <div class="item" id="ph">
+      <div class="item" id="ph" style="background-color: <?php echo($_SESSION["ItemsList"]["ph"] ? "#CCFFCC" : "#FFFFFF");  ?>">
         <p>
           <img class="item_pic_left" src="/images/phoenix.jpg">
           <h3>Phoenix - $1,000,000</h3>
           This is text describing a phoenix. Phoenixes are really cool!
         </p>
       </div>
-      <div class="item" id="hy">
+      <div class="item" id="hy" style="background-color: <?php echo($_SESSION["ItemsList"]["hy"] ? "#CCFFCC" : "#FFFFFF");  ?>">
         <p>
           <img class="item_pic_left" src="/images/hydra.jpg">
           <h3>Hydra - $1,000,000</h3>
@@ -29,7 +30,7 @@
         </p>
       </div>
     </div>
+    <button onclick="window.location.replace('cart.php');" value="Go to Cart">Go to Cart</button>
   </div>
-  <button onclick="window.location.replace('cart.php');" value="Go to Cart">Go to Cart</button>
 </body>
 </html>
