b4e2ec20d58ea73312f0ed5a53983185734b8b70
axle12693 <axle12693@gmail.com>
2019-05-11 14:16:28 -0600

    W3Prove

--- web/W03_Prove/index.php
+++ web/W03_Prove/index.php
@@ -1,4 +1,5 @@
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/header1.php'; ?>
+
     <script src="script.js"></script>
     <title>Alex's House of Mythical Pets</title>
 </head>
@@ -6,21 +7,21 @@
   <?php require $_SERVER['DOCUMENT_ROOT'] . '/header2.php'; ?>
   <div id="body_container">
     <div id="items_list_container">
-      <div class="item" name="dr">
+      <div class="item" id="dr">
         <p>
           <img class="item_pic_left" src="/images/dragon.jpg">
           <h3>Dragon - $1,000,000</h3>
           This is text describing a dragon. Dragons are really cool!
         </p>
       </div>
-      <div class="item" name="ph">
+      <div class="item" id="ph">
         <p>
           <img class="item_pic_left" src="/images/phoenix.jpg">
           <h3>Phoenix - $1,000,000</h3>
           This is text describing a phoenix. Phoenixes are really cool!
         </p>
       </div>
-      <div class="item" name="hy">
+      <div class="item" id="hy">
         <p>
           <img class="item_pic_left" src="/images/hydra.jpg">
           <h3>Hydra - $1,000,000</h3>
