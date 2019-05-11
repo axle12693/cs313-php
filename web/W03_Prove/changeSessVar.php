c310505ad9b0f5d39d05bc88c451a592cdae3c0d
axle12693 <axle12693@gmail.com>
2019-05-11 13:44:36 -0600

    W3Prove

--- web/W03_Prove/changeSessVar.php
+++ web/W03_Prove/changeSessVar.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 $var = $_POST['var'];
+if (!isset($_SESSION[$var]))
+{
+    $_SESSION[$var] = array();
+}
 $value = $_POST['value'];
 $_SESSION[$var][$value] = !$_SESSION[$var][$value];
 echo($_SESSION[$var][$value]);
