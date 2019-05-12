ea3ee5504b82336fd09622c97ca130e9d37b8b06
axle12693 <axle12693@gmail.com>
2019-05-11 21:44:14 -0600

    W3Prove

--- web/W03_Prove/cartTable.php
+++ web/W03_Prove/cartTable.php
@@ -6,16 +6,16 @@
         if ($buying)
         {
             $total += $costs[$item];
-            out = out . "<tr id=\"$item\" class=\"itemtr\">
+            $out = $out . "<tr id=\"$item\" class=\"itemtr\">
                   <td>$names[$item]</td>
                   <td>\$$costs[$item]</td>
                   <td><button value=\"Remove from cart\" id=\"$item\">Remove from cart</button></td>
                   </tr>";
         }
     }
-    out = out . "<tr>
+    $out = $out . "<tr>
           <td>Total:</td>
           <td>\$$total</td>
           </tr>";
-    echo(out);
+    echo($out);
 ?>
