4010a73b20c089f44e9538a304d44e7ceed3b3f9
axle12693 <axle12693@gmail.com>
2019-05-20 12:58:16 -0600

    Start DB access

--- web/forum_project/index.php
+++ web/forum_project/index.php
@@ -8,7 +8,7 @@ function add_forum_categories()
 
     $result = pg_execute($conn, "my_query", array());
 
-    echo(pg_fetch_all($result)[0][0]);
+    print_r(pg_fetch_all($result));
 }
 
 add_forum_categories();
