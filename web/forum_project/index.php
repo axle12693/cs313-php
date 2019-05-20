78cc2b8a21c09784ca2ebdc86e06b0a9f22d977b
axle12693 <axle12693@gmail.com>
2019-05-20 14:43:28 -0600

    Start DB access

--- web/forum_project/index.php
+++ web/forum_project/index.php
@@ -2,13 +2,22 @@
 
 function add_forum_categories()
 {
+    //Connect to DB
     $conn = pg_connect(getenv("DATABASE_URL"));
 
-    $result = pg_prepare($conn, "my_query", 'SELECT * FROM FORUM_CATEGORY');
+    //Get category IDs and titles
+    $result = pg_prepare($conn, "get_categories", 'SELECT * FROM FORUM_CATEGORY');
+    $result = pg_execute($conn, "get_categories", array());
+    $category_rows = pg_fetch_all($result);
 
-    $result = pg_execute($conn, "my_query", array());
-
-    print_r(pg_fetch_all($result));
+    //Get forum IDs and titles for each category
+    $forums = array();
+    for ($category_rows as $key => $value)
+    {
+        // $result = pg_prepare($conn, "get_forums", 'SELECT * FROM Forum WHERE forum_category_id = $1');
+        // $result = pg_execute($conn, "get_forums", array())
+        echo($key . " " . $value);
+    }
 }
 
 add_forum_categories();
