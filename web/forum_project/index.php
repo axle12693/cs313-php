28fa0efba9af9dfd6866564d1d532275163f3d9a
axle12693 <axle12693@gmail.com>
2019-05-20 12:23:59 -0600

    Start DB access

--- web/forum_project/index.php
+++ web/forum_project/index.php
@@ -2,17 +2,15 @@
 
 function add_forum_categories()
 {
-    echo(getenv("DATABASE_URL"));
+    $conn = pg_connect(getenv("DATABASE_URL"));
 
-    // $conn = pg_connect();
+    $result = pg_prepare($conn, "my_query", 'SELECT * FROM FORUM_CATEGORY');
 
-    // $result = pg_prepare($conn, "my_query", 'SELECT * FROM FORUM_CATEGORY');
+    $result = pg_execute($conn, "my_query", array());
 
-    // $result = pg_execute($conn, "my_query", array());
-
-    // echo($result)
+    echo($result);
 }
 
-add_forum_categories()
+add_forum_categories();
 
 ?>
