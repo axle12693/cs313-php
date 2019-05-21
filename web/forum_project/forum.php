5c0c39ab5107f36073df22e4ac499afbc8f85d22
axle12693 <axle12693@gmail.com>
2019-05-21 11:32:51 -0600

    Start DB access

--- web/forum_project/forum.php
+++ web/forum_project/forum.php
@@ -11,20 +11,7 @@ require("forum_functions.php");
         <?php
         $forum = htmlspecialchars($_GET["forum"]);
         $forum_title = htmlspecialchars($_GET["title"]);
-        $conn = pg_connect(getenv("DATABASE_URL"));
-        $result = pg_prepare($conn, "get_posts", '
-        SELECT      p.title, p.post_content, p.date_last_updated, au.username
-        FROM        Post p INNER JOIN App_User au 
-        ON          p.app_user_id = au.app_user_id
-        WHERE       forum_id = $1
-        ORDER BY    p.date_last_updated DESC
-        ');
-        $result = pg_execute($conn, "get_posts", array($forum));
-        $data_array = pg_fetch_all($result);
-        foreach ($data_array as $key => $value)
-        {
-            echo("Post " . $value["title"] . "<br>");
-        }
+        add_forum_posts();
         ?>  
     </div>
 </body>
