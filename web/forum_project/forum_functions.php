e3054548c944ac1a2b0fae4f3c1b135c4a199ba7
axle12693 <axle12693@gmail.com>
2019-05-21 11:40:30 -0600

    Start DB access

--- web/forum_project/forum_functions.php
+++ web/forum_project/forum_functions.php
@@ -31,13 +31,13 @@ function add_forum_categories()
 function add_forum_posts($forum_id)
 {
     $conn = pg_connect(getenv("DATABASE_URL"));
-        $result = pg_prepare($conn, "get_posts", '
+        $result = pg_prepare($conn, "get_posts", "
         SELECT      p.title, p.post_content, p.date_last_updated, au.username
         FROM        Post p INNER JOIN App_User au 
         ON          p.app_user_id = au.app_user_id
         WHERE       forum_id = $1
         ORDER BY    p.date_last_updated DESC
-        ');
+        ");
         $result = pg_execute($conn, "get_posts", array($forum));
         $data_array = pg_fetch_all($result);
         foreach ($data_array as $key => $value)
