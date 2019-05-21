37d9f4ae77730500346ab7004abfee892777c889
axle12693 <axle12693@gmail.com>
2019-05-21 08:28:37 -0600

    Start DB access

--- web/assignments.php
+++ web/assignments.php
@@ -5,8 +5,8 @@
   <?php require 'header2.php'; ?>
   <div id="body_container">
     <a href="week03_teach/index.php"> Week03 Teach</a><br>
-    <a href="W03_Prove/index.php">Week03 Prove</a>
-    <a href="W03_Prove/forum_project/index.php">Forum Project</a>
+    <a href="W03_Prove/index.php">Week03 Prove</a><br>
+    <a href="forum_project/index.php">Forum Project</a>
   </div>
 </body>
 </html>
