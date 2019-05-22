cdb6afa11e6541326e7767153af8109f7bef0c9a
axle12693 <axle12693@gmail.com>
2019-05-22 11:54:22 -0600

    Forum project

--- web/forum_project/forum_functions.php
+++ web/forum_project/forum_functions.php
@@ -21,7 +21,7 @@ function add_forum_categories()
         
         foreach ($forum_rows as $fkey => $fvalue)
         {
-            echo("<a class=\"card_body\" href=\"forum.php?forum=" . $fvalue["forum_id"] . "&title=" . $fvalue["title"] . "\">" . $fvalue["title"] . "</a>");
+            echo("<div class=\"card_body\" onclick=window.location.href=\"forum.php?forum=" . $fvalue["forum_id"] . "&title=" . $fvalue["title"] . "\">" . $fvalue["title"] . "</div>");
         }
 
         echo("</div>");
