9752e64ec4d73fba64bc15e13d1ca748e9388167
axle12693 <axle12693@gmail.com>
2019-05-23 16:24:14 -0600

    forum project

--- web/forum_project/forum_functions.php
+++ web/forum_project/forum_functions.php
@@ -98,8 +98,8 @@ function add_post_and_comments($post_id)
     echo("</div>");
 }
 
-$current_category = null;
-$current_forum = null;
+$current_category = Array();
+$current_forum = Array();
 
 function setup_current_forum_nav($forum_id)
 {
