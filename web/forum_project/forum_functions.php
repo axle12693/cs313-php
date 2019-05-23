35a1e294820c8d7ca71475cb705d7ff1b2c24e5f
axle12693 <axle12693@gmail.com>
2019-05-23 12:49:40 -0600

    forum project

--- web/forum_project/forum_functions.php
+++ web/forum_project/forum_functions.php
@@ -54,10 +54,37 @@ function add_forum_posts($forum_id)
             echo("<div class=\"card bg-info text-white\">");
         }
         $i+=1;
-        echo("<div class=\"card-body alink\" onclick=window.location.href=\"post.php?post=" . $value[""] . "\">");
+        echo("<div class=\"card-body alink\" onclick=window.location.href=\"post.php?post=" . $value["post_id"] . "\">");
         echo($value["title"] . " - " . $value["username"] . " - " . $value["date_last_updated"]);
         echo("</div></div>");
     }
+    echo("</div>");
+}
+
+function add_post_and_comments($post_id) 
+{
+    $conn = pg_connect(getenv("DATABASE_URL"));
+    $result = pg_prepare($conn, "get_post", "
+    SELECT      p.post_id, p.title, p.post_content, p.date_last_updated::date, au.username
+    FROM        Post p INNER JOIN App_User au 
+    ON          p.app_user_id = au.app_user_id
+    WHERE       p.post_id = $1
+    ");
+    $result = pg_execute($conn, "get_post", array($post_id));
+    $post_array = pg_fetch_all($result);
+
+    $result = pg_prepare($conn, "get_comments", "
+    SELECT      pc.post_comment_content, pc.app_user_id, pc.date_last_updated::date
+    FROM        Post p INNER JOIN Post_Content pc
+    ON          p.app_user_id = pc.app_user_id
+    WHERE       pc.post_id = $1
+    ORDER BY    pc.date_last_updated::date
+    ");
+    $result = pg_execute($conn, "get_comments", array($post_id));
+    $comment_array = pg_fetch_all($result);
+
+    echo("<div class=\"container\"");
+    echo("This is where the post and comments go!")
     echo("</div>");
 }
 
---
+++ web/forum_project/post.php
@@ -0,0 +1,17 @@
+<?php
+require($_SERVER['DOCUMENT_ROOT'] . '/header1.php');
+require("forum_functions.php");
+?>
+<title>Alex's House of Mythical Pets</title>
+</head>
+<body>
+    <?php require $_SERVER['DOCUMENT_ROOT'] . '/header2.php'; ?>
+    <div id="body_container">
+        <?php
+        $post = htmlspecialchars($_GET["post"]);
+        //$forum_title = htmlspecialchars($_GET["title"]);
+        add_post_and_comments($post);
+        ?>  
+    </div>
+</body>
+</html>
