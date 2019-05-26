c09d7356fc8fa61f2a6943bb664acad5716469ea
axle12693 <axle12693@gmail.com>
2019-05-25 23:03:41 -0600

    Forum project

--- web/forum_project/forum_functions.php
+++ web/forum_project/forum_functions.php
@@ -168,6 +168,7 @@ function try_login($uname, $pword)
     elseif (!password_verify($pword, data[0]["pw_hash"]))
     {
         echo(strlen(data[0]["pw_hash"]));
+        echo(data[0]["pw_hash"]);
         return false;
     }
     else
