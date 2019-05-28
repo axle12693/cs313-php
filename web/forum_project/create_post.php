0658c020f2f61c07dbb61deae2f3b32773edb699
axle12693 <axle12693@gmail.com>
2019-05-28 12:07:16 -0600

    forum_project

--- web/forum_project/create_post.php
+++ web/forum_project/create_post.php
@@ -13,15 +13,15 @@ require("forum_functions.php");
         $forum = htmlspecialchars($_REQUEST["forum"]);
         setup_current_forum_nav($forum);
         require("forum_header.php");
-        if (isset($_POST["title"]) && isset($_POST["post-content"]) && is_logged_in())
-        {
-            $conn = pg_connect(getenv("DATABASE_URL"));
-            $result = pg_prepare($conn, "create_post", "
-            INSERT INTO 
-            ");
-            $result = pg_execute($conn, "create_post", array($uname));
-            //redirect after everything
-        }
+        // if (isset($_POST["title"]) && isset($_POST["post-content"]) && is_logged_in())
+        // {
+        //     $conn = pg_connect(getenv("DATABASE_URL"));
+        //     $result = pg_prepare($conn, "create_post", "
+        //     INSERT INTO 
+        //     ");
+        //     $result = pg_execute($conn, "create_post", array($uname));
+        //     //redirect after everything
+        // }
 
         ?>  
         <form action="create_post.php" method="post">
