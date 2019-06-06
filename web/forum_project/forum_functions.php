458178fbcd22d0cc5b084b6509975de77efb51d4
axle12693 <axle12693@gmail.com>
2019-06-06 16:43:56 -0600

    edit/delete

--- web/forum_project/forum_functions.php
+++ web/forum_project/forum_functions.php
@@ -85,7 +85,7 @@ function is_allowed_to_edit_post($post_id)
 {
     $logged_in_user_id = get_logged_in_user_id();
     $creator_is_logged_in_user = get_post_creator($post_id)["app_user_id"] == $logged_in_user_id;
-    echo($logged_in_user_id . " . " . $creator_is_logged_in_user . " . " . get_post_creator($post_id)["app_user_id"]);
+    //echo($logged_in_user_id . " . " . $creator_is_logged_in_user . " . " . get_post_creator($post_id)["app_user_id"]);
     if ($creator_is_logged_in_user && $logged_in_user_id)
     {
         return true;
@@ -369,7 +369,7 @@ function edit_post($post_id, $title, $text)
             date_last_updated = current_timestamp
     WHERE   post_id = $1
     ");
-    $result = pg_execute($conn, "edit_post", array($post_id. $title, $text));
+    $result = pg_execute($conn, "edit_post", array($post_id, $title, $text));
 }
 
 ?>
